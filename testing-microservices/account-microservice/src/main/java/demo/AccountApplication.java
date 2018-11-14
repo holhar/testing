b006a18158918d8a0b8238b2272ac2d4adb77776
Holger Harms <h.harms@funkedigital.de>
2018-11-14 14:43:42 +0100

    formatting

--- testing-microservices/account-microservice/src/main/java/demo/AccountApplication.java
+++ testing-microservices/account-microservice/src/main/java/demo/AccountApplication.java
@@ -4,20 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * The {@link AccountApplication} is a
- * cloud-native Spring Boot application
- * that manages a bounded context for
- * @{link Customer},
- * @{link Account}, @{link CreditCard},
- * and @{link Address}
+ * The {@link AccountApplication} is a cloud-native Spring Boot application that manages a bounded context for
  *
  * @author Kenny Bastani
  * @author Josh Long
+ * @{link Customer},
+ * @{link Account}, @{link CreditCard}, and @{link Address}
  */
 @SpringBootApplication
 public class AccountApplication {
 
- public static void main(String[] args) {
-  SpringApplication.run(AccountApplication.class, args);
- }
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class, args);
+    }
 }
